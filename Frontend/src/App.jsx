9d0bfb00c31a7058990baf5ec8d16a7384bb6db9
Prakshal Jain <Prakshal.net12345@gmail.com>
2024-06-14 23:41:18 +0530

    responsive

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,14 @@
-export default function App() {
+import './App.css'
+import { Outlet } from 'react-router-dom'
+import Header from './components/header/Header'
+
+function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className='max-w-screen-xl mx-auto'>
+      <Header/>
+      <Outlet/>
+    </div>
   )
 }
+
+export default App
---
+++ Frontend/src/components/header/Header.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import DesktopNav from './DesktopNav';
+
+const Header = () => {
+    const [hideLeft,setHideLeft] = useState("-left-[1000px]");
+    const menuItems =["recipes","resources","about","contact"];
+
+    const onOpen=()=>{
+        setHideLeft("left-0");
+    }
+    const onClose=()=>{
+        setHideLeft("left-[1000px]");
+    }
+  return (
+    <div>
+        <div className='max-[900px]:hidden'>
+            <DesktopNav menuItems={menuItems}/>
+        </div>
+        <div className='min-[900px]:hidden'>MobileView</div>
+    </div>
+  )
+}
+
+export default Header
