4b6ea7b2ef13b6ad73e7950a93cdba59c64531e6
Prakshal Jain <Prakshal.net12345@gmail.com>
2024-06-15 15:14:58 +0530

    DesktopNav

--- Frontend/src/components/header/DesktopNav.jsx
+++ Frontend/src/components/header/DesktopNav.jsx
@@ -11,10 +11,21 @@ const DesktopNav = ({menuItems, Logo}) => {
             {
                 menuItems?.map((menu,index)=>(
                     <li key={index}>
-                        <Link to="/" className='font-medium capitalize text-secondary'>{menu}</Link>
+                        <Link to="{menu}" className='font-medium capitalize text-secondary'>{menu}</Link>
                     </li>
                 ))
             }
+        </ul>
+
+        {/* {Login and signup} */}
+
+        <ul className='flex items-center gap-4 font-medium'>
+            <li>
+                <button className='text-secondary px-4 py-2 rounded'>Login</button>
+            </li>
+            <li>
+                <button className='text-secondary px-4 py-2 rounded'>Sign Up</button>
+            </li>
         </ul>
     </div>
   )
